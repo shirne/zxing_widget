df3b5bfd11886593bb4b65597a89e5cf6d3110df
shirne <shirne@126.com>
2022-11-11 20:11:49 +0800

    example

---
+++ example/android/app/src/main/kotlin/com/shirne/zxing_widget/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shirne.zxing_widget.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:zxing_widget/zxing_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ZXing Widget Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'ZXing Widget Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      backgroundColor: Colors.grey[200],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: BarcodeWidget(
+                QrcodePainter('qrcode data'),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: BarcodeWidget(
+                QrcodePainter(
+                  'qrcode data',
+                  errorCorrectionLevel: ErrorCorrectionLevel.H,
+                  foregroundColor: Colors.blue,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: BarcodeWidget(
+                PDF417Painter('pdf417 data'),
+                size: const Size(300, 100),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: BarcodeWidget(
+                DataMatrixPainter('DataMatrix data'),
+                size: const Size(300, 200),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: BarcodeWidget(
+                AztecPainter('AZTec data'),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: BarcodeWidget(
+                OnedPainter('123456', Code93Writer()),
+                size: const Size(300, 100),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: BarcodeWidget(
+                OnedPainter('987654321', Code128Writer()),
+                size: const Size(350, 100),
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
